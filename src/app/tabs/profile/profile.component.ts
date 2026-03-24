85c1feb678e5ae1da21024fc0d44e97b6fb94580
Sourabh Mangalekar <sourabh@platformcommons.com>
2026-03-24 20:52:21 +0530

    Home page UI done

--- src/app/tabs/profile/profile.component.ts
+++ src/app/tabs/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  async onLogout() {
+    await this.authService.logout();
+    this.router.navigate(['/splash'], { replaceUrl: true });
+  }
 }
